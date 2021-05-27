80a8a99586ba66907e71d8d73cceea21d0dc5417
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-05-27 21:33:24 +0300

    add-countries-in-main-code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+import fetchCountries from './fetchCountries';
+const debounce = require('lodash.debounce');
+
+const formRef = document.querySelector('.form');
+const inputRef = document.querySelector('[data-input]');
+const listRef = document.querySelector('.list-js');
+
+inputRef.addEventListener('input', debounce(onInput, 500));
+
+
+function onInput(e) {
+    const countries = fetchCountries(e.target.value);
+    countries.then(console.log)
+}
