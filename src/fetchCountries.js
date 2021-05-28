f5d79956140ba97ca1e6af8cc2fe7f8c094d6342
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-05-28 12:12:27 +0300

    try-add-murkup

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,5 @@
 export default function fetchCountries(searchQuery) {
+    if (searchQuery === "") { return; };
     const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
 
     return fetch(url).then(resolve => resolve.json()).then(resolve => resolve);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import './sass/main.scss';
+import countriesTpl from './templates/list-countries.hbs'
 import fetchCountries from './fetchCountries';
 import { defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import { alert, notice, info, success, error } from '@pnotify/core';
 import * as PNotifyFontAwesome4 from '@pnotify/font-awesome4';
-
 defaultModules.set(PNotifyFontAwesome4, {});
-
-const myAlert = error({
-    text: "I'm an error.",
-    type: 'error',
-    autoOpen: false,
-    mode: 'dark',
-});
-
 const debounce = require('lodash.debounce');
 
 const formRef = document.querySelector('.form');
@@ -21,8 +13,37 @@ const listRef = document.querySelector('.list-js');
 
 inputRef.addEventListener('input', debounce(onInput, 500));
 
+const myAlert = error({
+    text: "I'm an error.",
+    type: 'error',
+    autoOpen: false,
+    mode: 'dark',
+});
 
 function onInput(e) {
+
     const countries = fetchCountries(e.target.value);
-    countries.then(console.log)
+    if (!countries) { return; };
+
+    countries.then(r => {
+        const countriesMarckp = countriesTpl(r);
+        listRef.insertAdjacentHTML('beforeend', countriesMarckp)
+        console.log();
+    });
+
 }
+
+function moreTenCountries(items) {
+    let errorIsOpen = false;
+
+    if (items.length >= 10) {
+        errorIsOpen = true;
+        myAlert.open();
+        console.log(errorIsOpen);
+
+    } else if (!errorIsOpen) {
+        myAlert.close();
+
+    };
+
+};
